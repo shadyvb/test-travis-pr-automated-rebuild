30c540bdc03ebe5fb9a165f68439ea867a242014
Shady Sharaf <shady@sharaf.me>
2017-11-13 17:18:39 +0200

    Unindent the comment

--- .github/probot.js
+++ .github/probot.js
@@ -1,8 +1,8 @@
 on('issues.opened')
   .comment(`
-    Hello @{{ sender.login }}. Thanks for inviting me to your project.
-    Read more about [all the things I can help you with][config]. I can't
-    wait to get started!
+Hello @{{ sender.login }}. Thanks for inviting me to your project.
+Read more about [all the things I can help you with][config]. I can't
+wait to get started!
 
-    [config]: https://github.com/bkeepers/workflow/blob/master/docs/configuration.md
-  `);
+[config]: https://github.com/bkeepers/workflow/blob/master/docs/configuration.md
+`);
