67512db2048563b8dc66f81ca6f2c6b11261c819
Shady Sharaf <shady@sharaf.me>
2017-11-17 18:53:46 +0200

    Update jarvis.js

--- .github/jarvis.js
+++ .github/jarvis.js
@@ -1,15 +1,3 @@
 on('issues.opened').filter( context => context.payload.issue.body.includes( 'Thank you' ) ).comment(`
 Hello @{{ sender.login }}. Thanks for submitting an issue, expect a reply within 48 seconds!
 `);
-
-on('issues.opened').filter( context => ! context.payload.issue.body.includes( 'Thank you' ) )
-.comment( 'Sorry @{{ sender.login }}, we do not accept issues that do not appreciate our work. You cannot ask requests unless you say "Thank you".')
-	.close();
-
-on('pull_request.opened').comment(
-'Sorry @{{ user.login }}, pull requests are not accepted on this repository. We develop our own shit!'
-).close();
-
-on('pull_request.closed').comment(
-'Just kidding @{{ user.login }}! Welcome aboard!'
-).close();
