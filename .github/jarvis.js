f778e66c8e3f2d804cb9ae703e6c44bbf9a9781f
Shady Sharaf <shady@sharaf.me>
2017-11-17 18:55:36 +0200

    Update jarvis.js

--- .github/jarvis.js
+++ .github/jarvis.js
@@ -1,3 +1,1 @@
-on('issues.opened').comment(`
-Hello @{{ sender.login }}. Thanks for submitting an issue, expect a reply within 48 seconds!
-`);
+on('issues.opened').comment(`Hello @{{ sender.login }}. Thanks for submitting an issue, expect a reply within 48 seconds!`);
