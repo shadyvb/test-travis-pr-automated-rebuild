2438abc6bb977768944772eccd812ea60610ee84
Shady Sharaf <shady@sharaf.me>
2017-11-17 18:55:20 +0200

    Update jarvis.js

--- .github/jarvis.js
+++ .github/jarvis.js
@@ -1,3 +1,3 @@
-on('issues.opened').filter( context => context.payload.issue.body.includes( 'Thank you' ) ).comment(`
+on('issues.opened').comment(`
 Hello @{{ sender.login }}. Thanks for submitting an issue, expect a reply within 48 seconds!
 `);
