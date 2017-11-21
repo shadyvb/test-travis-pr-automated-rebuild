1a3fe832995b6792ffa673a17768b65e93003b4d
Shady Sharaf <shady@sharaf.me>
2017-11-21 14:36:22 +0200

    Update jarvis.js

--- .github/jarvis.js
+++ .github/jarvis.js
@@ -22,3 +22,5 @@ on('pull_request.synchronize')
   .then( context => { 
     console.log( context.payload.pull_request.mergeable, context.payload.pull_request.mergeable_state )
   } )
+
+on('*').then( context => console.log('testing') )
