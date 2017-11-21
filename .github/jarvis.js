a6607556c7def31412990838eeabce5a819280eb
Shady Sharaf <shady@sharaf.me>
2017-11-21 17:23:26 +0200

    Update jarvis.js

--- .github/jarvis.js
+++ .github/jarvis.js
@@ -1,4 +1,4 @@
-/*on('pull_request.closed')
+on('pull_request.closed')
   .filter( context => context.payload.pull_request.base.ref == 'preprod' && context.payload.pull_request.merged )
   .then( context => {
     return context.github.pullRequests.create( context.repo({ 
@@ -8,22 +8,7 @@
     }) );
   });
 
-//on('pull_request').then( context => console.log(context.payload.action) )
-
-on('pull_request.synchronize')
-  .filter( context => ! context.payload.pull_request.mergeable )
-  .then( context => { 
-    return context.github.pullRequests.createComment( context.issue({
-      body: `Conflict detected, @${context.payload.pull_request.user.login} can you resolve that, please ?`
-    }));
-  } );
-*/
-on('pull_request.synchronize')
-  .then( context => { 
-    return context.github.pullRequests.update( context.issue({
-      body: `Conflict detected, @${context.payload.pull_request.user.login} can you resolve that, please ?`
-    }));
-  } );
+on('pull_request').then( context => console.log(context.payload.action) )
 
 on('issues.opened')
 .comment('Hello there');
