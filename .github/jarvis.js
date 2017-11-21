0277195107821a438a74c51c7334c42a68fa9ea6
Shady Sharaf <shady@sharaf.me>
2017-11-21 17:48:47 +0200

    Update jarvis.js

--- .github/jarvis.js
+++ .github/jarvis.js
@@ -1,15 +1,3 @@
-/*
-
-on('pull_request.closed')
-  .filter( context => context.payload.pull_request.base.ref == 'preprod' && context.payload.pull_request.merged )
-  .then( context => {
-    return context.github.pullRequests.create( context.repo({ 
-      title: '[Deploy] ' + context.payload.pull_request.title,
-      base: 'master',
-      head: context.payload.pull_request.head.ref
-    }) );
-  });
-*/
 on('pull_request.edited').then( context => {
   const title = context.payload.pull_request.title
   const isWip = /\bwip\b/i.test(title)
@@ -25,3 +13,15 @@ on('pull_request.edited').then( context => {
     context: 'WIP'
   }))
 } )
+
+/*
+on('pull_request.closed')
+  .filter( context => context.payload.pull_request.base.ref == 'preprod' && context.payload.pull_request.merged )
+  .then( context => {
+    return context.github.pullRequests.create( context.repo({ 
+      title: '[Deploy] ' + context.payload.pull_request.title,
+      base: 'master',
+      head: context.payload.pull_request.head.ref
+    }) );
+  });
+*/
